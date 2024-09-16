50d30d48849e680a717f38c205e9a22164139c8c
janobnotiq <janobnotiq@gmail.com>
2024-09-16 20:42:01 +0500

    django-unfold added, jazzmin removed

--- app/admin.py
+++ app/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from .models import Declaration,Company
+from unfold.admin import ModelAdmin
 
 # Register your models here.
 
 @admin.register(Company)
-class CompanyAdmin(admin.ModelAdmin):
+class CompanyAdmin(ModelAdmin):
     ordering = ["-created_at"]
 
 
 @admin.register(Declaration)
-class DeclarationAdmin(admin.ModelAdmin):
+class DeclarationAdmin(ModelAdmin):
     ordering = ["-created_at"]
